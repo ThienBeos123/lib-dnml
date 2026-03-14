316922dfb9f5c9ac742cddcf48ebfcf3d7df0b7c
admin <thien190911@gmail.com>
2026-03-15 01:30:50 +0700

    refactor(bigint): small refactoring of high-level wrappers temporary arena handling

--- big_numbers/bigInt.c
+++ big_numbers/bigInt.c
@@ -1480,19 +1480,16 @@ static void __BIGINT_MAGNITUDED_LCM__(bigInt *res, const bigInt *a, const bigInt
         .mark   = arena_mark_adapter,
         .reset  = arena_reset_adapter,
         .state = _DASI_MAGLCM_LARENA
-    }; size_t gcdres_mark = arena_mark(_DASI_MAGLCM_ARENA);
+    }; size_t maglcm_mark = arena_mark(_DASI_MAGLCM_ARENA);
     limb_t *gcdres_limbs = arena_alloc(_DASI_MAGLCM_ARENA, min(a->n, b->n));
-    size_t tmp_mark = arena_mark(_DASI_MAGLCM_ARENA);
     limb_t *tmp_limbs = arena_alloc(_DASI_MAGLCM_ARENA, min(a->n, b->n));
-    gcdres_limbs = _DASI_MAGLCM_ARENA->base += gcdres_mark;
     bigInt gcd_res = { .limbs = gcdres_limbs, /**/ .n = 0, /**/ .cap = min(a->n, b->n) }; 
     bigInt temp_rem = { .limbs = tmp_limbs, /**/ .n = 0, /**/ .cap = min(a->n, b->n) }; 
     __BIGINT_GCD_DISPATCH__(&gcd_res, a, b, _maglcm_ctx);
     __BIGINT_MAGNITUDED_DIVMOD__(res, &temp_rem, a, &gcd_res);
     __BIGINT_MAGNITUDED_MUL__(&gcd_res, res, b);
-    __BIGINT_MUT_COPY__(res, gcd_res);
-    arena_reset(_DASI_MAGLCM_ARENA, tmp_mark);
-    arena_reset(_DASI_MAGLCM_ARENA, gcdres_mark);
+    __BIGINT_MUT_COPY_OVER__(res, gcd_res);
+    arena_reset(_DASI_MAGLCM_ARENA, maglcm_mark);
 }
 static void __BIGINT_MAGNITUDED_EUCMOD_UI64__(uint64_t *res, const bigInt *a, uint64_t modulus) {
     uint64_t curr_rem = 0;
@@ -1547,10 +1544,9 @@ void __BIGINT_MUT_MUL_UI64__(bigInt *x, uint64_t val) {
         bigInt __TEMP_PROD__ = {
             .limbs  = tmp_limbs, /**/ .cap    = x->n + 1,
             .n      = 0,         /**/ .sign   = 1
-        };
-        __BIGINT_MAGNITUDED_MUL_UI64__(&__TEMP_PROD__, x, val);
+        }; __BIGINT_MAGNITUDED_MUL_UI64__(&__TEMP_PROD__, x, val);
         __BIGINT_MUT_COPY__(x, __TEMP_PROD__);
-        arena_reset(_DASI_MUL_UI64_ARENA, tmp_mark);
+        arena_reset(_DASI_MUL_UI64_ARENA, tmp_mark); _DASI_MUL_UI64_ARENA = NULL;
     }
 }
 dnml_status __BIGINT_MUT_DIV_UI64__(bigInt *x, uint64_t val) {
@@ -1568,10 +1564,9 @@ dnml_status __BIGINT_MUT_DIV_UI64__(bigInt *x, uint64_t val) {
             .n = 0,             /**/ .sign = 1 
         }; uint64_t temp_rem;
         __BIGINT_MAGNITUDED_DIVMOD_UI64__(&temp_quot, &temp_rem, x, val);
-        temp_quot.sign = x->sign;
-        __BIGINT_NORMALIZE__(&temp_quot);
+        temp_quot.sign = x->sign; __BIGINT_NORMALIZE__(&temp_quot);
         __BIGINT_MUT_COPY__(x, temp_quot);
-        arena_reset(_DASI_DIV_UI64_ARENA, tmp_mark);
+        arena_reset(_DASI_DIV_UI64_ARENA, tmp_mark); _DASI_DIV_UI64_ARENA = NULL;
     } return BIGINT_SUCCESS;
 }
 dnml_status __BIGINT_MUT_MOD_UI64__(bigInt *x, uint64_t val) {
@@ -1595,7 +1590,7 @@ dnml_status __BIGINT_MUT_MOD_UI64__(bigInt *x, uint64_t val) {
             x->limbs[0] = temp_rem;
             x->n        = (temp_rem) ? 1 : 0;
             x->sign     = (temp_rem) ? x->sign : 1;
-            arena_reset(_DASI_MOD_UI64_ARENA, tmp_mark);
+            arena_reset(_DASI_MOD_UI64_ARENA, tmp_mark); _DASI_MOD_UI64_ARENA = NULL;
         }
     } return BIGINT_SUCCESS;
 }
@@ -1613,9 +1608,9 @@ void __BIGINT_MUT_MUL_I64__(bigInt *x, int64_t val) {
             .limbs  = tmp_limbs,    /**/ .cap    = x->n + 1,
             .n      = 0,            /**/ .sign   = 1
         }; uint64_t mag_val = __MAG_I64__(val);
-        __BIGINT_MAGNITUDED_MUL_UI64__(&__TEMP_PROD__, x, mag_val);
+        __BIGINT_MAGNITUDED_MUL_UI64__(&__TEMP_PROD__, x, mag_val); 
         __BIGINT_MUT_COPY__(x, __TEMP_PROD__);
-        arena_reset(_DASI_MUL_I64_ARENA, tmp_mark);
+        arena_reset(_DASI_MUL_I64_ARENA, tmp_mark); _DASI_MUL_I64_ARENA = NULL;
     } x->sign *= (val < 0) ? -1 : 1;
 }
 dnml_status __BIGINT_MUT_DIV_I64__(bigInt *x, int64_t val) {
@@ -1634,9 +1629,8 @@ dnml_status __BIGINT_MUT_DIV_I64__(bigInt *x, int64_t val) {
         }; uint64_t temp_rem, mag_val = __MAG_I64__(val);
         __BIGINT_MAGNITUDED_DIVMOD_UI64__(&temp_quot, &temp_rem, x, mag_val);
         temp_quot.sign = x->sign * ((val < 0) ? -1 : 1);
-        __BIGINT_NORMALIZE__(&temp_quot);
-        __BIGINT_MUT_COPY__(x, temp_quot);
-        arena_reset(_DASI_DIV_I64_ARENA, tmp_mark);
+        __BIGINT_NORMALIZE__(&temp_quot); __BIGINT_MUT_COPY__(x, temp_quot);
+        arena_reset(_DASI_DIV_I64_ARENA, tmp_mark); _DASI_DIV_I64_ARENA = NULL;
     } return BIGINT_SUCCESS;
 }
 dnml_status __BIGINT_MUT_MOD_I64__(bigInt *x, int64_t val) {
@@ -1660,7 +1654,7 @@ dnml_status __BIGINT_MUT_MOD_I64__(bigInt *x, int64_t val) {
             x->limbs[0] = temp_rem;
             x->n        = (temp_rem) ? 1 : 0;
             x->sign     = (temp_rem) ? x->sign : 1;
-            arena_reset(_DASI_MOD_I64_ARENA, tmp_mark);
+            arena_reset(_DASI_MOD_I64_ARENA, tmp_mark); _DASI_MOD_I64_ARENA = NULL;
         }
     } return BIGINT_SUCCESS;
 }
@@ -1680,9 +1674,8 @@ void __BIGINT_MUT_ADD__(bigInt *x, const bigInt y) {
             .n     = 0,         /**/ .sign  = 1
         };
         __BIGINT_MAGNITUDED_ADD__(&temp_sum, x, &y);
-        temp_sum.sign = x->sign;
-        __BIGINT_MUT_COPY__(x, temp_sum);
-        arena_reset(_DASI_ADD_ARENA, tmp_mark);
+        temp_sum.sign = x->sign; __BIGINT_MUT_COPY__(x, temp_sum);
+        arena_reset(_DASI_ADD_ARENA, tmp_mark); _DASI_ADD_ARENA = NULL;
     } else { 
         int8_t comp_res = __BIGINT_COMPARE_MAGNITUDE__(x, &y);
         if (!comp_res) __BIGINT_RESET__(x);
@@ -1696,7 +1689,7 @@ void __BIGINT_MUT_ADD__(bigInt *x, const bigInt y) {
             if (comp_res > 0)   { __BIGINT_MAGNITUDED_SUB__(&temp_sum, x, &y); temp_sum.sign = x->sign; }
             else                { __BIGINT_MAGNITUDED_SUB__(&temp_sum, x, &y); temp_sum.sign = y.sign; }
             __BIGINT_MUT_COPY__(x, temp_sum);
-            arena_reset(_DASI_ADD_ARENA, tmp_mark);
+            arena_reset(_DASI_ADD_ARENA, tmp_mark); _DASI_ADD_ARENA = NULL;
         }
     }
 }
@@ -1719,7 +1712,7 @@ void __BIGINT_MUT_SUB__(bigInt *x, const bigInt y) {
             if (comp_res > 0) { __BIGINT_MAGNITUDED_SUB__(&temp_diff, x, &y); temp_diff.sign = x->sign; }
             else              { __BIGINT_MAGNITUDED_SUB__(&temp_diff, x, &y); temp_diff.sign = -x->sign; }
             __BIGINT_MUT_COPY__(x, temp_diff);
-            arena_reset(_DASI_SUB_ARENA, tmp_mark);
+            arena_reset(_DASI_SUB_ARENA, tmp_mark); _DASI_SUB_ARENA = NULL;
         }
     } else { dnml_arena *_DASI_SUB_ARENA = _USE_ARENA();
         size_t tmp_mark = arena_mark(_DASI_SUB_ARENA);
@@ -1729,9 +1722,8 @@ void __BIGINT_MUT_SUB__(bigInt *x, const bigInt y) {
             .n     = 0,         /**/ .sign  = 1
         };
         __BIGINT_MAGNITUDED_ADD__(&temp_diff, x, &y);
-        temp_diff.sign = x->sign;
-        __BIGINT_MUT_COPY__(x, temp_diff);
-        arena_reset(_DASI_SUB_ARENA, tmp_mark);
+        temp_diff.sign = x->sign; __BIGINT_MUT_COPY__(x, temp_diff);
+        arena_reset(_DASI_SUB_ARENA, tmp_mark); _DASI_SUB_ARENA = NULL;
     }
 }
 void __BIGINT_MUT_MUL__(bigInt *x, const bigInt y) {
@@ -1748,9 +1740,8 @@ void __BIGINT_MUT_MUL__(bigInt *x, const bigInt y) {
             .limbs = tmp_limbs, /**/ .cap   = x->n * y.n,
             .n     = 0,         /**/ .sign  = 1
         };
-        __BIGINT_MAGNITUDED_MUL__(&__TEMP_PROD__, x, &y);
-        __BIGINT_MUT_COPY__(x, __TEMP_PROD__);
-        arena_reset(_DASI_MUL_ARENA, tmp_mark);
+        __BIGINT_MAGNITUDED_MUL__(&__TEMP_PROD__, x, &y); __BIGINT_MUT_COPY__(x, __TEMP_PROD__);
+        arena_reset(_DASI_MUL_ARENA, tmp_mark); _DASI_MUL_ARENA = NULL;
     } x->sign *= y.sign;
 }
 dnml_status __BIGINT_MUT_DIV__(bigInt *x, const bigInt y) {
@@ -1762,24 +1753,14 @@ dnml_status __BIGINT_MUT_DIV__(bigInt *x, const bigInt y) {
     else if (y.n == 1 && y.limbs[0] == 1) x->sign *= y.sign;
     else if (x->n == 1 && x->limbs[0] == 1) __BIGINT_RESET__(x);
     else { dnml_arena *_DASI_DIV_ARENA = _USE_ARENA();
-        size_t quot_mark = arena_mark(_DASI_DIV_ARENA); // Always at the start, not need to find aligned offset
+        size_t mutdiv_mark = arena_mark(_DASI_DIV_ARENA);
         limb_t *quot_limbs = arena_alloc(_DASI_DIV_ARENA, BYTES_IN_UINT64_T * x->n);
         limb_t *rem_limbs = arena_alloc(_DASI_DIV_ARENA, BYTES_IN_UINT64_T * y.n);
-        size_t rem_mark = arena_mark(_DASI_DIV_ARENA) - (BYTES_IN_UINT64_T * y.n); // Get the aligned offset
-        bigInt temp_quot = {
-            .limbs = quot_limbs, /**/ .cap   = x->n,
-            .n     = 0,          /**/ .sign  = 1
-        }; 
-        bigInt temp_rem = {
-            .limbs = rem_limbs, /**/ .cap   = y.n,
-            .n     = 0,         /**/ .sign  = 1
-        };
-        __BIGINT_MAGNITUDED_DIVMOD__(&temp_quot, &temp_rem, x, &y);
-        temp_quot.sign = x->sign * y.sign;
-        __BIGINT_NORMALIZE__(&temp_quot);
-        __BIGINT_MUT_COPY__(x, temp_quot);
-        arena_reset(_DASI_DIV_ARENA, rem_mark); 
-        arena_reset(_DASI_DIV_ARENA, quot_mark);
+        bigInt temp_quot = {.limbs = quot_limbs, .sign = 1,     /**/    .cap = x->n, .n = 0,}; 
+        bigInt temp_rem = {.limbs = rem_limbs, .sign = 1,       /**/    .cap = y.n, .n = 0,};
+        __BIGINT_MAGNITUDED_DIVMOD__(&temp_quot, &temp_rem, x, &y); temp_quot.sign = x->sign * y.sign;
+        __BIGINT_NORMALIZE__(&temp_quot); __BIGINT_MUT_COPY__(x, temp_quot);
+        arena_reset(_DASI_DIV_ARENA, mutdiv_mark); _DASI_DIV_ARENA = NULL;
     } return BIGINT_SUCCESS;
 }
 dnml_status __BIGINT_MUT_MOD__(bigInt *x, const bigInt y) {
@@ -1795,23 +1776,14 @@ dnml_status __BIGINT_MUT_MOD__(bigInt *x, const bigInt y) {
         else if (!comp_res) __BIGINT_RESET__(x);
         else { dnml_arena *_DASI_MOD_ARENA = _USE_ARENA();
             // Always at the start, no need to find the aligned offset
-            size_t quot_mark = arena_mark(_DASI_MOD_ARENA);
+            size_t mutmod_mark = arena_mark(_DASI_MOD_ARENA);
             limb_t *quot_limbs = arena_alloc(_DASI_MOD_ARENA, BYTES_IN_UINT64_T * x->n);
             limb_t *rem_limbs = arena_alloc(_DASI_MOD_ARENA, BYTES_IN_UINT64_T * y.n);
-            size_t rem_mark = arena_mark(_DASI_MOD_ARENA) - (BYTES_IN_UINT64_T * y.n); // Get the aligned offset
-            bigInt temp_quot = {
-                .limbs = quot_limbs,    /**/ .cap   = x->n,
-                .n     = 0,             /**/ .sign  = 1
-            }; 
-            bigInt temp_rem = {
-                .limbs = rem_limbs, /**/ .cap   = y.n,
-                .n     = 0,         /**/ .sign  = 1
-            };
+            bigInt temp_quot = {.limbs = quot_limbs, .sign = 1,     /**/    .cap = x->n, .n = 0}; 
+            bigInt temp_rem = {.limbs = rem_limbs, .sign = 1,       /**/    .cap = y.n, .n = 0};
             __BIGINT_MAGNITUDED_DIVMOD__(&temp_quot, &temp_rem, x, &y);
-            temp_rem.sign = x->sign;
-            __BIGINT_MUT_COPY__(x, temp_rem);
-            arena_reset(_DASI_MOD_ARENA, rem_mark); 
-            arena_reset(_DASI_MOD_ARENA, quot_mark);
+            temp_rem.sign = x->sign; __BIGINT_MUT_COPY__(x, temp_rem);
+            arena_reset(_DASI_MOD_ARENA, mutmod_mark); _DASI_MOD_ARENA = NULL;
         }
     } return BIGINT_SUCCESS;
 }
@@ -1869,10 +1841,10 @@ bigInt __BIGINT_MOD_UI64__(const bigInt x, uint64_t val, dnml_status *err) {
             rem.limbs[0] = temp_rem;
             rem.n        = (temp_rem) ? 1 : 0;
             rem.sign     = (temp_rem) ? x.sign : 1;
-            arena_reset(_DASI_FMOD_UI64_ARENA, tmp_mark);
+            arena_reset(_DASI_FMOD_UI64_ARENA, tmp_mark); 
+            _DASI_FMOD_UI64_ARENA = NULL;
         }
-    } 
-    *err = BIGINT_SUCCESS;
+    } *err = BIGINT_SUCCESS;
     return rem;
 }
 bigInt __BIGINT_MUL_I64__(const bigInt x, int64_t val) {
@@ -1933,6 +1905,7 @@ bigInt __BIGINT_MOD_I64__(const bigInt x, int64_t val, dnml_status *err) {
             rem.n        = (temp_rem) ? 1 : 0;
             rem.sign     = x.sign;
             arena_reset(_DASI_FMOD_I64_ARENA, tmp_mark);
+            _DASI_FMOD_I64_ARENA = NULL;
         }
     }
     *err = BIGINT_SUCCESS;
@@ -2003,11 +1976,9 @@ bigInt __BIGINT_DIV__(const bigInt x, const bigInt y, dnml_status *err) {
         bigInt temp_rem = {
             .limbs = tmp_limbs, /**/ .cap   = y.n,
             .n     = 0,         /**/ .sign  = 1
-        };
-        __BIGINT_MAGNITUDED_DIVMOD__(&quot, &temp_rem, &x, &y);
-        quot.sign = x.sign * y.sign;
-        __BIGINT_NORMALIZE__(&quot);
-        arena_reset(_DASI_FDIV_ARENA, tmp_mark);
+        }; __BIGINT_MAGNITUDED_DIVMOD__(&quot, &temp_rem, &x, &y);
+        quot.sign = x.sign * y.sign; __BIGINT_NORMALIZE__(&quot);
+        arena_reset(_DASI_FDIV_ARENA, tmp_mark); _DASI_FDIV_ARENA = NULL;
     } *err = BIGINT_SUCCESS; 
     return quot;
 }
@@ -2031,11 +2002,9 @@ bigInt __BIGINT_MOD__(const bigInt x, const bigInt y, dnml_status *err) {
             bigInt temp_quot = {
                 .limbs = tmp_limbs, /**/ .cap   = x.n,
                 .n     = 0,         /**/ .sign  = 1
-            };
-            __BIGINT_MAGNITUDED_DIVMOD__(&temp_quot, &rem, &x, &y);
-            rem.sign = x.sign;
-            __BIGINT_NORMALIZE__(&rem);
-            arena_reset(_DASI_FMOD_ARENA, tmp_mark);
+            }; __BIGINT_MAGNITUDED_DIVMOD__(&temp_quot, &rem, &x, &y);
+            rem.sign = x.sign; __BIGINT_NORMALIZE__(&rem);
+            arena_reset(_DASI_FMOD_ARENA, tmp_mark); _DASI_FMOD_ARENA = NULL;
         }
     } *err = BIGINT_SUCCESS;
     return rem;
@@ -2063,7 +2032,8 @@ bigInt __BIGINT_GCD_UI64__(const bigInt x, uint64_t val) {
             .cap = 1, /**/ .sign = 1
         }; y.limbs[0] = val;
         __BIGINT_MAGNITUDED_GCD__(&res, &x, &y);
-        arena_reset(_DASI_UI64_ARENA, tmp_mark); tmp_limbs = NULL;
+        arena_reset(_DASI_UI64_ARENA, tmp_mark);
+        tmp_limbs = NULL; _DASI_UI64_ARENA = NULL;
     } return res;
 }
 bigInt __BIGINT_GCD_I64__(const bigInt x, int64_t val) {
@@ -2082,7 +2052,8 @@ bigInt __BIGINT_GCD_I64__(const bigInt x, int64_t val) {
             .cap = 1, /**/ .sign = 1
         }; y.limbs[0] = __MAG_I64__(val);
         __BIGINT_MAGNITUDED_GCD__(&res, &x, &y);
-        arena_reset(_DASI_UI64_ARENA, tmp_mark); tmp_limbs = NULL;
+        arena_reset(_DASI_UI64_ARENA, tmp_mark); 
+        tmp_limbs = NULL; _DASI_UI64_ARENA = NULL;
     } return res;
 }
 bigInt __BIGINT_GCD__(const bigInt x, const bigInt y) {
@@ -2114,6 +2085,7 @@ bigInt __BIGINT_LCM_UI64__(const bigInt x, uint64_t val) {
         }; y.limbs[0] = val;
         __BIGINT_MAGNITUDED_LCM__(&res, &x, &y);
         arena_reset(_DASI_LCM_UI64_ARENA, tmp_mark);
+        _DASI_LCM_UI64_ARENA = NULL;
     } return res;
 }
 bigInt __BIGINT_LCM_I64__(const bigInt x, int64_t val) {
@@ -2134,6 +2106,7 @@ bigInt __BIGINT_LCM_I64__(const bigInt x, int64_t val) {
         }; y.limbs[0] = mag_val;
         __BIGINT_MAGNITUDED_LCM__(&res, &x, &y);
         arena_reset(_DASI_LCM_UI64_ARENA, tmp_mark);
+        _DASI_LCM_UI64_ARENA = NULL;
     }
 }
 bigInt __BIGINT_LCM__(const bigInt x, const bigInt y) {
@@ -2211,6 +2184,7 @@ dnml_status __BIGINT_MUT_MODULO__(bigInt *x, const bigInt modulus) {
             __BIGINT_MAGNITUDED_SUB__(&tmp_res, &modulus, &tmp_res);
             __BIGINT_MAGNITUDED_EUCMOD__(&tmp_res, &tmp_res, &modulus);
         } __BIGINT_INTERNAL_COPY__(x, &tmp_res);
+        arena_reset(_DASI_MUT_MODULO_ARENA, tmp_mark); _DASI_MUT_MODULO_ARENA = NULL;
     } return BIGINT_SUCCESS;
 }
 uint64_t __BIGINT_MODULO_UI64__(const bigInt x, uint64_t modulus, dnml_status *err) {
