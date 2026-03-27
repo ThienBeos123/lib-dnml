dd829f8a85b6c8d6ca71771b03d197b7cd6ff9cd
admin <thien190911@gmail.com>
2026-03-27 23:24:32 +0700

    feat(arm64): finished 64 bit wide multiplication for a 128 bit product in arm64 assembly

--- intrinsics/arm64/_arm64_conn.h
+++ intrinsics/arm64/_arm64_conn.h
@@ -6,8 +6,14 @@
 #include "../../system/compiler.h"
 
 #if __ARCH_ARM64__
+// _arm64_arith.S
 extern uint64_t _arm64_add64c(uint64_t a, uint64_t b, uint8_t *carry);
 extern uint64_t _arm64_sub64b(uint64_t a, uint64_t b, uint8_t *borrow);
+extern uint64_t _arm64_wmul128(uint64_t a, uint64_t b, uint64_t *hi);
+// _arm64_modop.S
+
+// _arm64_utils.S
+
 #endif
 
 
