6090c1f190bdfbf4fdb399a673c985feb6be023d
admin <thien190911@gmail.com>
2026-03-30 22:43:26 +0700

    feat(x86): finished bitwise operations - clz, ctz, bswap - in intel x86_64 system-v abi assembly

--- intrinsics/x86_64/_x86_conn.h
+++ intrinsics/x86_64/_x86_conn.h
@@ -5,6 +5,7 @@
 #include "../../system/compiler.h"
 #include "../../system/arch.h"
 
+//* -------- SYSTEM-V ABI ---------- *//
 #if __ABI_X64_SYSV__
 // _x86_sysv_arith.S
 extern uint64_t _x86sv_add64c(uint64_t a, uint64_t b, uint8_t *carry);
@@ -21,13 +22,25 @@ extern uint64_t _x86sv_wdiv128(
 // _x86_sysv_marith.S
 extern uint64_t _x86sv_modinv64(uint64_t x);
 #define _x86_modinv64   _x86sv_modinv64
-// _x86_sys_valg.S
+// _x86_sysv_alg.S
 // _x86_sysv_bitops.S
+extern uint64_t _x86sv_clz64(uint64_t x);
+extern uint64_t _x86sv_ctz64(uint64_t x);
+extern uint64_t _x86sv_bswap64(uint64_t x);
+#define _x86_clz64      _x86sv_clz64
+#define _x86_ctz64      _x86sv_ctz64
+#define _x86_bswap64    _x86sv_bswap64
+
+
+//* -------- WINDOW-64 ABI ---------- *//
 #elif __ABI_X64_WIN64__
 // _x86_win64_arith.S
 // _x86_win64_marith.S
 // _x86_win64_alg.S
 // _x86_win64_bitops.S
+
+
+//* -------- UNKNOWN ABI ---------- *//
 #else
 #include "../zvanillc/_vanillc_conn.h"
 #define _x86_add64c     _cintrin_add64c
