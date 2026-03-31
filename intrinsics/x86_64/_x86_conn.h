985bd27e696206f51ca9ffc0e3375c750c2b402b
admin <thien190911@gmail.com>
2026-03-31 14:54:19 +0700

    feat(win64): finished clz, ctz, bswap in intel x86_64 assembly for win64 abi

--- intrinsics/x86_64/_x86_conn.h
+++ intrinsics/x86_64/_x86_conn.h
@@ -37,9 +37,26 @@ extern uint64_t _x86sv_bswap64(uint64_t x);
 //* -------- WINDOW-64 ABI ---------- *//
 #elif __ABI_X64_WIN64__
 // _x86_win64_arith.S
+extern uint64_t _x86w64_add64c(uint64_t a, uint64_t b, uint8_t *carry);
+extern uint64_t _x86w64_sub64b(uint64_t a, uint64_t b, uint8_t *borrow);
+extern uint64_t _x86w64_wmul128(uint64_t a, uint64_t b, uint64_t *hi);
+extern uint64_t _x86w64_wdiv128(
+    uint64_t lo, uint64_t hi,
+    uint64_t div, uint64_t *rem
+);
+#define _x86_add64c     _x86w64_add64c
+#define _x86_sub64b     _x86w64_sub64b
+#define _x86_wmul128    _x86w64_wmul128
+#define _x86_wdiv128    _x86w64_wdiv128
 // _x86_win64_marith.S
 // _x86_win64_alg.S
 // _x86_win64_bitops.S
+extern uint64_t _x86w64_clz64(uint64_t x);
+extern uint64_t _x86w64_ctz64(uint64_t x);
+extern uint64_t _x86w64_bswap64(uint64_t x);
+#define _x86_clz64      _x86w64_clz64
+#define _x86_ctz64      _x86w64_ctz64
+#define _x86_bswap64    _x86w64_bswap64
 
 
 //* -------- UNKNOWN ABI ---------- *//
